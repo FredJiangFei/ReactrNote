036ae2cd4df7cc1082ff5ad1cfde8604dcf32524
Fred Jiang <jiangf@shinetechsoftware.com>
2022-04-15 09:23:30 +0800

    diff test

--- react-diff/src/__tests__/NodeList.tsx
+++ react-diff/src/__tests__/NodeList.tsx
@@ -1,9 +1,33 @@
 import { diff } from '../types/NodeList';
 
-test('1', () => {
-  const before = [{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }];
-  const after = [{ key: 'd' }, { key: 'a' }, { key: 'b' }, { key: 'c' }];
+test('test delete and add', () => {
+  const before = [{ key: 'a' }];
+  const after = [{ key: 'b' }];
 
   const result = diff(before, after);
-  console.log(result);
+
+  expect(result).toHaveLength(2);
+
+  var nodeA = result.find((x) => x.key === 'a');
+  expect(nodeA?.flag).toBe('Deletion');
+
+  var nodeB = result.find((x) => x.key === 'b');
+  expect(nodeB?.flag).toBe('Placement');
+});
+
+test('test move', () => {
+  const before = [{ key: 'a' }, { key: 'b' }, { key: 'c' }];
+  const after = [{ key: 'c' }, { key: 'b' }, { key: 'a' }];
+
+  const result = diff(before, after);
+
+  expect(result).toHaveLength(2);
+
+  var nodeA = result.find((x) => x.key === 'a');
+  expect(nodeA?.flag).toBe('Placement');
+  expect(nodeA?.index).toBe(2);
+
+  var nodeB = result.find((x) => x.key === 'b');
+  expect(nodeB?.flag).toBe('Placement');
+  expect(nodeB?.index).toBe(1);
 });
